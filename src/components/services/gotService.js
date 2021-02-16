7e590eeb0df62dc75420c004fccb2622fca28a88
Loboda Vladyslav <vladoxer280502@gmail.com>
2021-02-16 20:39:06 +0200

    randomchar

--- src/components/services/gotService.js
+++ src/components/services/gotService.js
@@ -13,21 +13,61 @@ export default class GotService {
         return await res.json();
     }
 
-    getAllCharacter() {
-        return this.getResource("/characters?page=5&pageSize=10");
+    async getAllCharacter() {
+        const res = await this.getResource("/characters?page=5&pageSize=10");
+        return res.map(this._transformCharacter)
     }
 
-    getCharacter(id) {
-        return this.getResource(`/characters/${id}`);
+    async getCharacter(id) {
+        const character = await this.getResource(`/characters/${id}`);
+        return this._transformCharacter(character);
 
+    }
+
+    getAllHouse(id) {
+        return this.getResource(`/characters/${id}`);
+    }
+
+    getHouses() {
+        return this.getResource(`/houses`);
+    }
+
+    getAllBooks() {
+        return this.getResource(`/books`);
+    }
+
+    getBook(id) {
+        return this.getResource(`/books/${id}`)
+    }
+
+    _transformCharacter(char) {
+        return {
+            name: char.name || 'no info',
+            gender: char.gender || 'no info',
+            born: char.born || 'no info',
+            died: char.died || 'no info',
+            culture: char.culture || 'no info',
+        }
+    }
+
+    _tansformHouse(house) {
+        return {
+            name: house.name,
+            region: house.region,
+            words: house.words,
+            titles: house.titles,
+            overlord: house.overlord,
+            ancestralWeapons: house.ancestralWeapons
+        }
+    }
+
+    _transformBook(book) {
+        return {
+            name: book.name,
+            numberOfPages: book.numberOfPages,
+            publiser: book.publiser,
+            released: book.released
+        }
     }
 }
 
-const got = new GotService();
-
-got.getAllCharacter()
-    .then(res => {
-        res.forEach(item => console.log(item.name));
-    });
-got.getCharacter(130)
-    .then(res => console.log(res));
---
+++ src/components/spinner/spinner.js
@@ -0,0 +1,41 @@
+import React from "react";
+import './spinner.css'
+
+const Spinner = () => {
+    return (
+        <div className="loadingio-spinner-spin-tnklb6j6zbr">
+            <div className="ldio-j0vu4rmbkn">
+                <div>
+                    <div></div>
+                </div>
+                <div>
+                    <div></div>
+                </div>
+                <div>
+                    <div></div>
+                </div>
+                <div>
+                    <div></div>
+                </div>
+                <div>
+                    <div></div>
+                </div>
+                <div>
+                    <div></div>
+                </div>
+                <div>
+                    <div></div>
+                </div>
+                <div>
+                    <div></div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+
+export default Spinner;
+
+
+
